e9c37198a88fe0737323a2a8f1a5c83c186ea04b
MJSanfelippo <mj.sanfelippo@gmail.com>
2017-11-11 20:23:34 -0800

    Got it to successfully delete TextViews (made up some random ones) on a long press via contextmenu

--- app/src/main/java/android/csulb/edu/cecs453finalproject/MainActivity.java
+++ app/src/main/java/android/csulb/edu/cecs453finalproject/MainActivity.java
@@ -3,9 +3,13 @@ package android.csulb.edu.cecs453finalproject;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.view.ViewGroup.LayoutParams;
@@ -14,7 +18,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout foodList;
+    private int id;
+    private LinearLayout foodList;
+    TextView viewClicked;
 
     TextView goalCalories;
     TextView currentCalories;
@@ -22,12 +28,21 @@ public class MainActivity extends AppCompatActivity {
 
     Button addFoodButton;
 
-    public void addNew(){
+    public void addNew(int id){
         TextView test = new TextView(getApplicationContext());
         test.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-        test.setText("Tester");
+        test.setText("Tester" + id);
+        test.setId(id++);
+        registerForContextMenu(test);
         int dp = Formulas.convertToDp(20, getResources().getDisplayMetrics().density);
         test.setPadding(dp,dp,dp,dp);
+        test.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                return false;
+            }
+        });
         foodList.addView(test);
     }
 
@@ -35,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        id = 0;
         getSupportActionBar().setTitle("Health Buddy");
         foodList = (LinearLayout) findViewById(R.id.foodList);
 
@@ -43,8 +59,34 @@ public class MainActivity extends AppCompatActivity {
         remainingCalories = (TextView) findViewById(R.id.remainingCalories);
 
         addFoodButton = (Button) findViewById(R.id.addFoodButton);
+        addNew(5);
+        addNew(6);
+        addNew(7);
+
+
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("options");
+        viewClicked = (TextView) v;
+
+        menu.add(0, 1, 0, "edit");
+        menu.add(0, 2, 1, "delete");
+
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+
+        if (item.getTitle().toString().equals("delete")){
+            Toast.makeText(getApplicationContext(), "this is: " + viewClicked.getText().toString(), Toast.LENGTH_SHORT).show();
+            foodList.removeView(viewClicked);
+        } else {
+            Toast.makeText(getApplicationContext(), "this is: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onContextItemSelected(item);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
